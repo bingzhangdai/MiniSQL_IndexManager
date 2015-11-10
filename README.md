cc06a798a22054a7003be8cf0cc4603e83d7063d
bingzhang <bingzhangdai@gmail.com>
2015-11-10 18:59:20 +0800

    replace \t with 4 spaces

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SQL_IndexManager
 Project of database design course.<br/>
 
-It is one of 5 modules of <a title="our whole project" href="https://github.com/2015DatabaseDesign/MiniDB" target="_blank">MINISQL </a> <br/>
+It is one of 6 modules of <a title="our whole project" href="https://github.com/2015DatabaseDesign/MiniDB" target="_blank">MINISQL </a> <br/>
 ## description
 IndexManager class provides 4 interfaces for other modules<br/>
 
@@ -10,11 +10,11 @@ class IndexManager {
 private:
     /* some code here */
 public:
-	IndexManager();
-	int search_one(const std::string& database, const std::string& table_name, struct index_info& inform);
-	void search_many(const std::string& database, const std::string& table_name, int type, struct index_info& inform, std::vector<int>& container);
-	void insert_one(const std::string& database, const std::string& table_name, struct index_info& inform);
-	void delete_one(const std::string& database, const std::string& table_name, struct index_info& inform);
+    IndexManager();
+    int search_one(const std::string& database, const std::string& table_name, struct index_info& inform);
+    void search_many(const std::string& database, const std::string& table_name, int type, struct index_info& inform, std::vector<int>& container);
+    void insert_one(const std::string& database, const std::string& table_name, struct index_info& inform);
+    void delete_one(const std::string& database, const std::string& table_name, struct index_info& inform);
 };
 ```
 ## datatypes
--- mystr.cpp
+++ mystr.cpp
@@ -3,87 +3,87 @@
 using namespace std;
 
 int StrToI(const string& s) {
-	int m = 0, sign = 0;
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			m *= 10;
-			m += s[i] - '0';
-		}
-		else
-			sign = 1;
-	}
-	if (sign)
-		return -m;
-	return m;
+    int m = 0, sign = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            m *= 10;
+            m += s[i] - '0';
+        }
+        else
+            sign = 1;
+    }
+    if (sign)
+        return -m;
+    return m;
 }
 
 const string IToStr(int n, int length) {
-	int sign = 0;
-	if (n < 0) {
-		n = -n;
-		sign = 1;
-	}
-	char *s = (char *)malloc(sizeof(char) * (length + 1));
-	s[length] = '\0';
-	for (int i = length - 1; i >= 0; i--) {
-		if (n == 0 && sign) {
-			s[i] = '-';
-			sign = 0;
-		}
-		else
-			s[i] = n % 10 + '0';
-		n /= 10;
-	}
-	string ss(s);
-	free(s);
-	return ss;
+    int sign = 0;
+    if (n < 0) {
+        n = -n;
+        sign = 1;
+    }
+    char *s = (char *)malloc(sizeof(char) * (length + 1));
+    s[length] = '\0';
+    for (int i = length - 1; i >= 0; i--) {
+        if (n == 0 && sign) {
+            s[i] = '-';
+            sign = 0;
+        }
+        else
+            s[i] = n % 10 + '0';
+        n /= 10;
+    }
+    string ss(s);
+    free(s);
+    return ss;
 }
 
 int FindFirstNon0(const string& s) {
-	int i;
-	for (i = 0; i < s.size(); i++)
-		if (s[i] != '0')
-			break;
-	return i;
+    int i;
+    for (i = 0; i < s.size(); i++)
+        if (s[i] != '0')
+            break;
+    return i;
 }
 
 int IntCmp(const string& s, const string& t) {
-	int i = FindFirstNon0(s);
-	int j = FindFirstNon0(t);
-	int n = atoi(s.substr(i, s.size() - i).c_str());
-	int m = atoi(t.substr(j, t.size() - j).c_str());
-	return n - m;
+    int i = FindFirstNon0(s);
+    int j = FindFirstNon0(t);
+    int n = atoi(s.substr(i, s.size() - i).c_str());
+    int m = atoi(t.substr(j, t.size() - j).c_str());
+    return n - m;
 }
 
 int FloatCmp(const string& s, const string& t) {
-	int i = FindFirstNon0(s);
-	int j = FindFirstNon0(t);
-	float n = atof(s.substr(i, s.size() - i).c_str());
-	float m = atof(t.substr(j, t.size() - j).c_str());
-	if (n < m)
-		return -1;
-	else if (n > m)
-		return 1;
-	else
-		return 0;
+    int i = FindFirstNon0(s);
+    int j = FindFirstNon0(t);
+    float n = atof(s.substr(i, s.size() - i).c_str());
+    float m = atof(t.substr(j, t.size() - j).c_str());
+    if (n < m)
+        return -1;
+    else if (n > m)
+        return 1;
+    else
+        return 0;
 }
 
 int StrCmp(const string& s, const string& t) {
-	int i = FindFirstNon0(s);
-	int j = FindFirstNon0(t);
-	string n = s.substr(i, s.size() - i).c_str();
-	string m = t.substr(j, t.size() - j).c_str();
-	if (n < m)
-		return -1;
-	else if (n > m)
-		return 1;
-	else
-		return 0;
+    int i = FindFirstNon0(s);
+    int j = FindFirstNon0(t);
+    string n = s.substr(i, s.size() - i).c_str();
+    string m = t.substr(j, t.size() - j).c_str();
+    if (n < m)
+        return -1;
+    else if (n > m)
+        return 1;
+    else
+        return 0;
 }
 
 
 const std::string SpanStrToLen(std::string s, int length) {
-	while (s.size() < length)
-		s = "0" + s;
-	return s;
+    while (s.size() < length)
+        s = "0" + s;
+    return s;
 }
